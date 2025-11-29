e429f9c1a891a5b8e35027dc86fae803618672a1
Jennah17422 <126196794+Jennah17422@users.noreply.github.com>
2025-11-29 13:51:40 -0800

    Create Module10_main.cpp

---
+++ Module10_main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // Module 10
+using namespace std;
+#include "User.h"
+#include "Employee.h"
+#include "InventoryManager.h"
+
+
+int main() {
+    InventoryManager mgr;
+    mgr.accessLevel(); // Prints all of the inventory management access
+
+    return 0;
+}
